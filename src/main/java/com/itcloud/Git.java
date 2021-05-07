0e00f15b0028e0b031a68db5d6a81e1229163c1c
wuam <wuam@qq.com>
2021-05-07 23:26:06 +0800

    third commit

--- src/main/java/com/itcloud/Git.java
+++ src/main/java/com/itcloud/Git.java
@@ -7,6 +7,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("this is Git!");
         System.out.println("this is Git two!");
+        System.out.println("this is Git three!");
     }
 
 }
