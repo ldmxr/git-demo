2af8a60e8f445cedf3bbdaa1840af7392fd68f54
wuam <wuam@qq.com>
2021-05-08 22:10:20 +0800

    seventh commit

--- src/main/java/com/itcloud/GitTest.java
+++ src/main/java/com/itcloud/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("this is Git three!");
         System.out.println("this is Hot-fix!");
         System.out.println("Hot-fix test!");
+        System.out.println("first github commit!");
     }
 }
